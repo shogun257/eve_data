b7924fa16f9b3e0a1f90245d6a6d3015aa19c98f
jens <User@User-PC>
2017-03-21 14:04:13 +0100

    things

--- crest_get_orders.php
+++ crest_get_orders.php
@@ -4,8 +4,8 @@ ini_set("max_execution_time", "180");
 require_once 'classes/db_classes/pdo_eve.php';
 
 
-$region_ids[] = '10000002'; #The Forge
-#$region_ids[] = '10000013'; #malpais
+#$region_ids[] = '10000002'; #The Forge
+$region_ids[] = '10000013'; #malpais
 foreach ($region_ids as $region_id) {
     $url = 'https://crest-tq.eveonline.com/market/' . $region_id . '/orders/all/';
     get_market_by_url($url);
@@ -16,13 +16,13 @@ function get_market_by_url($url) {
     $jsonarray = json_decode($jsonfile);
 
     echo '<pre>';
-    print_r($jsonarray);
+    #print_r($jsonarray);
     foreach ($jsonarray->items as $key => $value) {
         #print_r($value);
         $sql = "INSERT INTO `eve`.`tbl_order` (`id`, `buy`, `issued`, `price`, `volume`, `duration`, `min_volume`, `range`, `station_id`, `type`) VALUES"
                 . " ('" . $value->id . "', '" . $value->buy . "', '" . $value->issued . "', '" . $value->price . "', '" . $value->volume . "', '" . $value->duration . "', '" . $value->minVolume . "', '" . $value->range . "', '" . $value->stationID . "', '" . $value->type . "');";
-        #echo "<br>" . $sql . "<br>";
-        #pdo_eve::mysql_query($sql);
+        echo "<br>" . $sql . "<br>";
+        pdo_eve::mysql_query($sql);
     }
     echo '<br>' . $jsonarray->next->href . '<br>';
     get_market_by_url($jsonarray->next->href);
